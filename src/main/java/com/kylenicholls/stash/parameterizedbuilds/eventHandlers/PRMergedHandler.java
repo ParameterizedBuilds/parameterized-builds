93d5fdadee0344ca94102c307c40a21ede3cda8b
Austin Hacker <explosivepyrounicorns@gmail.com>
2018-12-12 13:24:42 -0500

    null check the merge commit as remotely merged PRs will be null (#167)

--- src/main/java/com/kylenicholls/stash/parameterizedbuilds/eventHandlers/PRMergedHandler.java
+++ src/main/java/com/kylenicholls/stash/parameterizedbuilds/eventHandlers/PRMergedHandler.java
@@ -1,11 +1,14 @@
 package com.kylenicholls.stash.parameterizedbuilds.eventHandlers;
 
+import com.atlassian.bitbucket.commit.MinimalCommit;
 import com.atlassian.bitbucket.event.pull.PullRequestMergedEvent;
 import com.atlassian.bitbucket.pull.PullRequestService;
 import com.kylenicholls.stash.parameterizedbuilds.ciserver.Jenkins;
 import com.kylenicholls.stash.parameterizedbuilds.helper.SettingsService;
 import com.kylenicholls.stash.parameterizedbuilds.item.BitbucketVariables;
 import com.kylenicholls.stash.parameterizedbuilds.item.Job;
+
+import java.util.Optional;
 
 public class PRMergedHandler extends PRHandler{
 
@@ -14,7 +17,9 @@ public class PRMergedHandler extends PRHandler{
     public PRMergedHandler(SettingsService settingsService, PullRequestService pullRequestService, Jenkins jenkins,
                            PullRequestMergedEvent event, String url){
         super(settingsService, pullRequestService, jenkins, event, url, Job.Trigger.PRMERGED);
-        this.mergeCommit = event.getCommit().getId();
+        this.mergeCommit = Optional.ofNullable(event.getCommit())
+                .map(MinimalCommit::getId)
+                .orElse("");
     }
 
     @Override
