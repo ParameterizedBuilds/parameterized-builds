292d30fbe39759140db8ae3b7e5e3ba6989c7511
a-hacker <austin.hacker@datarobot.com>
2022-01-03 16:24:37 -0500

    Add tests

--- src/test/java/com/kylenicholls/stash/parameterizedbuilds/item/BitbucketVariablesTest.java
+++ src/test/java/com/kylenicholls/stash/parameterizedbuilds/item/BitbucketVariablesTest.java
@@ -34,6 +34,7 @@ public class BitbucketVariablesTest {
     private final Long PR_ID = 15L;
     private final String PR_DESCRIPTION = "Description of this PR";
     private final String PR_AUTHOR = "this guy";
+    private final String PR_USERNAME = "this_guy";
     private final String PR_EMAIL = "example@domain.com";
     private final String SOURCE_BRANCH = "sourcebranch";
     private final String DEST_BRANCH = "destbranch";
@@ -65,6 +66,7 @@ public class BitbucketVariablesTest {
         when(branch.getLatestCommit()).thenReturn(COMMIT);
         when(author.getUser()).thenReturn(user);
         when(user.getDisplayName()).thenReturn(PR_AUTHOR);
+        when(user.getName()).thenReturn(PR_USERNAME);
         when(user.getEmailAddress()).thenReturn(PR_EMAIL);
         when(prFromRef.getRepository()).thenReturn(forkRepository);
         when(prFromRef.getDisplayId()).thenReturn(SOURCE_BRANCH);
@@ -140,6 +142,14 @@ public class BitbucketVariablesTest {
                 .populateFromPR(pullRequest, repository, projectKey, trigger, url).build();
 
         assertEquals(PR_AUTHOR, actual.fetch("$PRAUTHOR"));
+    }
+
+    @Test
+    public void testPopulateFromPRSetsPRUsername() {
+        BitbucketVariables actual = new BitbucketVariables.Builder()
+                .populateFromPR(pullRequest, repository, projectKey, trigger, url).build();
+
+        assertEquals(PR_USERNAME, actual.fetch("$PRUSERNAME"));
     }
 
     @Test
