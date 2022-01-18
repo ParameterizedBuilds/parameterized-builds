d613cf103f6a85a04ac9ba0414e9991dedd39851
a-hacker <austin.hacker@datarobot.com>
2022-01-17 23:09:50 -0500

    Check for null user on trigger

--- src/main/java/com/kylenicholls/stash/parameterizedbuilds/ciserver/JenkinsConnection.java
+++ src/main/java/com/kylenicholls/stash/parameterizedbuilds/ciserver/JenkinsConnection.java
@@ -70,7 +70,7 @@ public class JenkinsConnection {
         }
 
         String buildUrl = job.buildUrl(jenkinsServer, bitbucketVariables, false);
-        boolean prompt = !user.getSlug().equals(jenkinsServer.getUser());
+        boolean prompt = user == null || !user.getSlug().equals(jenkinsServer.getUser());
 
         String csrfHeader = null;
         if (jenkinsServer.getCsrfEnabled()) {
